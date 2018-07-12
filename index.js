421df26303ffd459192ac815ec3b25941cb911c9
Yuriko Hasebe <yuriko.hasebe@yurikohase-ltm.internal.salesforce.com>
2018-07-12 21:31:00 +0900

    commit

--- index.js
+++ index.js
@@ -22,19 +22,17 @@ const client = new Client({
 });
 client.connect();
 
-// Make a request for a user with a given ID
-axios.get('http://localhost:5050/?:accountid')
-  .then(function (req,res) {
+// Make a request for a accountid with a given ID
+axios.get('http://localhost:5050/?accountid=112233')
+  .then(function (res) {
     // handle success
     console.log("________req.params_________ ")
-    console.log(req.params);
-    console.log("________err.status_________ ")
-    console.log(err.status);
+    console.log(res.params);
     console.log("_________________ ")
   });
 
-// Make a request for a user with a given ID
-axios.get('https://mutb.herokuapp.com/?:accountid')
+// Make a request for a accountid with a given ID
+axios.get('https://mutb.herokuapp.com/?accountid=112233')
   .then(function (req,res) {
     // handle success
     console.log("________req.params_________ ")
