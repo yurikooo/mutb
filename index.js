f7f415f0b664bf8aeb7228d65065dbbeb7f989ad
Yuriko Hasebe <yuriko.hasebe@yurikohase-ltm.internal.salesforce.com>
2018-07-13 09:48:23 +0900

    param commit

--- index.js
+++ index.js
@@ -67,10 +67,28 @@ app.get('/',function (req, res){
           res.render('index', {connectResults: result.rows});
         });
 
+      //間違った企業IDの場合のエラーハンドリング
       }else{
+         query = 'SELECT * FROM salesforce.SNS__c order by salesforce.SNS__c.id desc limit 2';
+        if(query){
+          var result = [];
+          client.query(query, function(err, result){
+            res.render('index', {connectResults: result.rows}); 
+          });
+        }       
+
         //res.redirect('/');
       }
 
+    //企業IDがない場合のエラーハンドリング
+    }else{
+        query = 'SELECT * FROM salesforce.SNS__c order by salesforce.SNS__c.id desc limit 2';
+        if(query){
+          var result = [];
+          client.query(query, function(err, result){
+            res.render('index', {connectResults: result.rows}); 
+          });
+        }
     }
 
 //    var query = 'SELECT * FROM salesforce.SNS__c where salesforce.SNS__c.accountid__c = \'' + param.accountid + '\' order by salesforce.SNS__c.id desc limit 2';
